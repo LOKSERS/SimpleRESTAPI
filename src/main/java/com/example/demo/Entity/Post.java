b8c655ea8ccf15ebea7a28353f1373592be215fa
Dmitrii Ganichev <Dmtr.Lks@yandex.ru>
2021-07-13 00:20:54 +0300

    Added Entity and DB Connection

---
+++ src/main/java/com/example/demo/Entity/Post.java
@@ -0,0 +1,40 @@
+package com.example.demo.Entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+
+@Data
+@Entity
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String caption;
+    private String location;
+    private Integer likes;
+
+    @Column
+    @ElementCollection(targetClass = String.class)
+    private Set<String> likedUsers = new HashSet<>();
+    @ManyToOne(fetch = FetchType.LAZY)
+    private  User user;
+    @OneToMany(cascade = CascadeType.REFRESH,fetch = FetchType.EAGER,mappedBy = "post",orphanRemoval = true)
+    private List<Comment> comments = new LinkedList<>();
+    @Column(updatable = false)
+    private LocalDateTime createdDate;
+
+    @PrePersist
+    protected void onCreate(){
+        this.createdDate = LocalDateTime.now();
+    }
+
+}
---
+++ src/main/java/com/example/demo/Entity/enums/ERole.java
@@ -0,0 +1,5 @@
+package com.example.demo.Entity.enums;
+
+public enum ERole {
+    ROLE_USER, ROLE_ADMIN
+}
